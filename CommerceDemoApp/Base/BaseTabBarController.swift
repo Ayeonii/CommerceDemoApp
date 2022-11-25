502a5b86e56236fad3681025dfd6e9b15447d062
Ayeonii <dldkekdusz@naver.com>
2022-11-25 15:27:31 +0900

    [feat] 탭바 생성 및 화면 연결

--- CommerceDemoApp/Base/BaseTabBarController.swift
+++ CommerceDemoApp/Base/BaseTabBarController.swift
@@ -8,7 +8,33 @@
 import UIKit
 
 class BaseTabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.setupTabBar()
+    }
+    
+    func setupTabBar() {
+        let homeIcon = UITabBarItem(title: "홈",
+                                    image: UIImage(systemName: "house"),
+                                    selectedImage: UIImage(systemName: "house.fill")!)
+        let likeIcon = UITabBarItem(title: "좋아요",
+                                    image: UIImage(systemName: "heart"),
+                                    selectedImage: UIImage(systemName: "heart.fill")!)
+        
+        tabBar.tintColor = .appColor(.deepRosePink)
+        
+        let homeVC = HomeViewController()
+        homeVC.hidesBottomBarWhenPushed = false
+        let homeNavigation = UINavigationController(rootViewController: homeVC)
+        homeNavigation.tabBarItem = homeIcon
+        
+        let likeVC = LikeViewController()
+        likeVC.hidesBottomBarWhenPushed = false
+        let likeNavigation = UINavigationController(rootViewController: homeVC)
+        likeNavigation.tabBarItem = likeIcon
+        
+        self.viewControllers = [homeNavigation, likeNavigation]
     }
 }
