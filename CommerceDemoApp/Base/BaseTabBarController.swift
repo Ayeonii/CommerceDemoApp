5fafec3226ce696e5261175450210cef101f9ffb
Ayeonii <dldkekdusz@naver.com>
2022-11-25 14:53:39 +0900

    [File] 탭바 및 Page 디렉토리 생성

---
+++ CommerceDemoApp/Base/BaseTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseTabBarController.swift
+//  CommerceDemoApp
+//
+//  Created by 이아연 on 2022/11/25.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
